0d3ba76b7697a1d263dedad2cbf28c66fb66382d
elenaJO <ele.lucero1993@gmail.com>
2021-05-08 13:28:17 -0500

    feat(MAQ): maquetacion checkbox y radio button

---
+++ src/components/shared/RadioBtnApp.tsx
@@ -0,0 +1,16 @@
+export const RadioBtnApp = () => {
+	return (
+		<div className="radio-btn-app">
+			<input
+				className="radio-btn-app__radio"
+				type="radio" 
+				id="check2"
+			/>
+    	<label
+				className="radio-btn-app__label"
+				htmlFor="check2"
+				>si
+			</label>
+		</div>
+	)
+}
