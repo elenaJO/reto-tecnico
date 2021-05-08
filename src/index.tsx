5d18ec48acef451987a29d5cfd5abbcf21e67fc8
elenaJO <ele.lucero1993@gmail.com>
2021-05-08 18:20:27 -0500

    feat(MAQ): maquetacion pasos

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/styles.scss'
-import { HomeScreen } from './components/home/HomeScreen';
+// import { HomeScreen } from './components/home/HomeScreen';
 import { HeaderApp } from './components/shared/HeaderApp';
+import { AutoDataScreen } from './components/auto-data/AutoDataScreen';
 
 ReactDOM.render(
   <React.StrictMode>
     <HeaderApp/>
-    <HomeScreen/>
+    {/* <HomeScreen/> */}
+    <AutoDataScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
