a52d3e55e445c1bacb4b3db65f39e716b18fbbaa
elenaJO <ele.lucero1993@gmail.com>
2021-05-08 12:28:27 -0500

    feat(MAQ): maquetaccion inputs y selects

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 
 import './styles/styles.scss'
 import { BtnApp } from './components/shared/buttons/BtnApp';
+import { InputApp } from './components/shared/InputApp';
+import { SelectApp } from './components/shared/SelectApp';
 
 ReactDOM.render(
   <React.StrictMode>
     <BtnApp title={'COTÍZALO'}/>
+    <InputApp/>
+    <SelectApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
