24ecbcf92624e939f6ae452805084820bca7c6d1
elenaJO <ele.lucero1993@gmail.com>
2021-05-09 10:07:24 -0500

    feat(MAQ): maquetacion agrega/quita cobertura

--- src/components/arm-plan/ArmPlanScreen.tsx
+++ src/components/arm-plan/ArmPlanScreen.tsx
@@ -1,11 +1,13 @@
 import { AutoDataSteps } from "../auto-data/AutoDataSteps"
 import { ArmPlanHeader } from "./ArmPlanHeader"
+import { ArmPlanOffOn } from "./ArmPlanOffOn"
 
 export const ArmPlanScreen = () => {
 	return (
 		<>
 			<AutoDataSteps/>
 			<ArmPlanHeader/>
+			<ArmPlanOffOn/>
 		</>
 	)
 }
---
+++ src/components/shared/SwitchApp.tsx
@@ -0,0 +1,10 @@
+export const SwitchApp = () => {
+	return (
+		<div className="switch-app">
+			<label>
+				<input type="checkbox" className="switch-app__input"/>
+				<span className="switch-app__slider switch-app__round"></span>
+			</label>
+		</div>
+	)
+}
