a586fb0b4bbe97a88807d7e312c2c35e1348032e
elenaJO <ele.lucero1993@gmail.com>
2021-05-10 21:03:31 -0500

    feat(INT): integracion consumo de servicio

---
+++ src/actions/auth.ts
@@ -0,0 +1,28 @@
+import { types } from "../components/types/types";
+
+export const startRegister = (documentNumber: string, phone: string, license: string) => {
+	return async (dispatch) => {
+		const url = 'https://jsonplaceholder.typicode.com/users/1';
+		const resp = await fetch(url);
+		const data = await resp.json();
+		const { name, email } = data;
+		dispatch(login(documentNumber, phone, license, name, email));
+	}
+}
+
+export const login = (
+	documentNumber: string,
+	phone: string,
+	license: string,
+	name: string,
+	email: string,
+	) => ({
+	type: types.login,
+	payload: {
+		documentNumber,
+		phone,
+		license,
+		name,
+		email
+	}
+});
--- src/components/reducers/authReducer.ts
+++ src/components/reducers/authReducer.ts
@@ -5,7 +5,11 @@ export const authReducer = (state = {}, action: any) => {
 	switch (action.type) {
 		case types.login:
 			return {
-				id: action.payload.id,
+				documentNumber: action.payload.documentNumber,
+				phone: action.payload.phone,
+				license: action.payload.license,
+				name: action.payload.name,
+				email: action.payload.email,
 			}	
 		default:
 			return state;
