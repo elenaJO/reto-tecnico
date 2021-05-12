efd6ec1cd7f72fe90b9f1f12264bacbe824c4fc3
elenaJO <ele.lucero1993@gmail.com>
2021-05-11 19:01:01 -0500

    feat(INT): implementacion contador

--- src/components/types/types.ts
+++ src/components/types/types.ts
@@ -1,4 +1,5 @@
 export const types = {
   login: '[Auth] login',
+  setSumAssured: '[Mount] setSumAssured',
 }
   
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { authReducer } from '../components/reducers/authReducer';
+import { amountReducer } from '../components/reducers/amountReducer';
 
 const composeEnhancers = (typeof window !== 'undefined' && window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']) || compose;
 
 const reducers = combineReducers({
 	auth: authReducer,
+	amount: amountReducer, 
 });
 
 export const store = createStore(
