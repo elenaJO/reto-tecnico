d329c9390e8f2af1f9efbdc732432dd401cc05c3
elenaJO <ele.lucero1993@gmail.com>
2021-05-12 19:00:31 -0500

    feat(MAQ): maquetacion footer

---
+++ src/components/shared/FooterApp.tsx
@@ -0,0 +1,7 @@
+export const FooterApp = () => {
+	return (
+		<div className="footer-app">
+			<span className="footer-app__text">© 2020 RIMAC Seguros y Reaseguros.</span>
+		</div>
+	)
+}
--- src/components/welcome/WelcomeScreen.tsx
+++ src/components/welcome/WelcomeScreen.tsx
@@ -1,9 +1,11 @@
+import { FooterApp } from "../shared/FooterApp"
 import { WelcomeBanner } from "./WelcomeBanner"
 
 export const WelcomeScreen = () => {
 	return (
 		<>
-			<WelcomeBanner/>		
+			<WelcomeBanner/>
+			<FooterApp/>		
 		</>
 	)
 }
